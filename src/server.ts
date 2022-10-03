2cba445209d5e0a30bfbcf7bb1f6331963e89a15
Leonardo Spuldar <spdleonardo@hotmail.com>
2022-10-03 20:24:24 -0300

    typeorm

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("Server is running!"));
